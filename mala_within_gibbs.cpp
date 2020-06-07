77b4f34e608b8b6597850e1f7f6dcbabb2e99ab8
Midhun Unnikrishnan <gslotnik@gmail.com>
2020-06-07 15:03:52 +0100

    writing namespace

--- mala_within_gibbs.cpp
+++ mala_within_gibbs.cpp
@@ -8,48 +8,48 @@
 #include "standard_functionals.h"
 
 // function inputs
-Tnum logpi_f1(std::array<Tnum, 2> v)
+Mala::Tnum logpi_f1(std::array<Mala::Tnum, 2> v)
 {
-	const Tnum b = 0.01;
-	Tnum &x = v[0], &y = v[1];
-	Tnum f1 = x / 10.0;
-	Tnum f2 = (y + b * x*x + 100.0 * b);
+	const Mala::Tnum b = 0.01;
+	Mala::Tnum &x = v[0], &y = v[1];
+	Mala::Tnum f1 = x / 10.0;
+	Mala::Tnum f2 = (y + b * x*x + 100.0 * b);
 	return -f1 * f1 - f2 * f2;
 }
 
 int main() {
-	// input func
+	// model inputs
 	constexpr long unsigned dim_m = 1; // size of each block
 	constexpr long unsigned dim_n = 2; // number of blocks
 	constexpr long unsigned dim = dim_m*dim_n;
-	sfunc<dim> logpi = logpi_f1;
+	Mala::sfunc<dim> logpi = logpi_f1;
 
 	// model params
-	const Tnum tau = 0.1;
+	const Mala::Tnum tau = 0.1;
 	const size_t B = 1e5;
 	const size_t N = 1e6;
 
 	// rng
-	std::normal_distribution<Tnum> normal(0, 1);
-	std::uniform_real_distribution<Tnum> uniform(0, 1);
+	std::normal_distribution<Mala::Tnum> normal(0, 1);
+	std::uniform_real_distribution<Mala::Tnum> uniform(0, 1);
 	std::default_random_engine e(std::time(0));
-	auto rgen = [&normal,&e]()->Tnum{ return normal(e); };
+	auto rgen = [&normal,&e]()->Mala::Tnum{ return normal(e); };
 
 	// init
 	const auto sqrt2tau = sqrt(2 * tau);
 
 	// model output
-	Tnum I = 0;
-	Tnum a = 0;
+	Mala::Tnum I = 0;
+	Mala::Tnum a = 0;
 
 	// containers
-	nvec<dim> x = {0}, y = {0};
-	nvec<dim_m> x_j, y_j, innov_j, gradlog_j;
-	Tnum logprop = 0;
+	Mala::nvec<dim> x = {0}, y = {0};
+	Mala::nvec<dim_m> x_j, y_j, innov_j, gradlog_j;
+	Mala::Tnum logprop = 0;
 
 	// generated funcs
-	auto gradlogpi = grad_numeric_blocks<dim,dim_m>(logpi);
-	auto lq = logq_blocks<dim,dim_m>(gradlogpi, tau);
+	auto gradlogpi = Mala::grad_numeric_blocks<dim,dim_m>(logpi);
+	auto lq = Mala::logq_blocks<dim,dim_m>(gradlogpi, tau);
 	
 	// Record start time
 	auto start = std::chrono::high_resolution_clock::now();
@@ -60,17 +60,17 @@ int main() {
 			gradlog_j = gradlogpi[j](x);
 			
 			// \xi^k_j
-	    std::generate_n(begin(innov_j), dim_m, rgen);
+	    std::generate_n(std::begin(innov_j), dim_m, rgen);
 
 			// x^k_j
-			std::copy_n(begin(x) + j*dim_m, dim_m, begin(x_j) );			
+			std::copy_n(std::begin(x) + j*dim_m, dim_m, std::begin(x_j) );			
 	    
 			// candidate block y^k_j
 			y_j = x_j + tau * gradlog_j + sqrt2tau * innov_j;
 			
 			// candidate whole y^k
 			y = x;
-			std::copy_n(begin(y_j), dim_m, begin(y) + j*dim_m);			
+			std::copy_n(std::begin(y_j), dim_m, std::begin(y) + j*dim_m);			
 			
 			logprop = logpi(y) + lq[j](y, x) - logpi(x) - lq[j](x, y);
 			if (log(uniform(e)) < logprop)
